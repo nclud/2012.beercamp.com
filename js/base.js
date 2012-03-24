b40778ab454e99270d29450f711b2f1789aaf0e8
John Schulz <JFSIII@JFSIII.org>
2012-03-24 11:33:45 -0400

    Move `Popup` into `base.js`
    Eliminate duplicate functions (`degToRad`, `radToDeg`), take advantage of other conveniences like `clamp` and `cssTransformProperty`

--- js/base.js
+++ js/base.js
@@ -471,7 +471,7 @@ function craftThatPaperBaby() {
 		var $popup = $(this);
 		var master = $popup.parents('.spread');
 		var depth = $popup.attr('data-depth');
-		var popup = new Popup($popup, depth);
+		var popup = new Popup($popup, depth, paperFolding);
 		popups.push(popup);
 	});
 
@@ -494,5 +494,57 @@ function hideLocationBar() {
 		setTimeout(function () {
 			window.scrollTo(0, 1);
 		}, 0);
+	}
+}
+
+function Popup(graphic, depth, event) {
+	if (!event) event = 'pageFolding';
+	var master = graphic.parents('.spread'),
+		zRot = graphic.parents('.page').hasClass('page-left') ? 15 : -15,
+		POPUP_WIDTH = 300;
+	
+	master.bind(event, function (e, per) {
+		var fold = clamp(per, 0);
+		setFold(fold);
+	});
+	
+	setFold(1);
+
+	
+	function setFold(fold) {
+		var points = [];
+		
+		// origin
+		points[0] = [0, 0, 0];
+		
+		// left piece: bottom outside
+		points[1] = [-POPUP_WIDTH * Math.cos(degToRad(-180 * fold)), POPUP_WIDTH * Math.sin(degToRad(-180 * fold)), POPUP_WIDTH * Math.sin(degToRad(-15))];
+		
+		// right piece: bottom outside
+		points[2] = [POPUP_WIDTH * Math.cos(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-15))];
+		
+		// left piece: top inside
+		points[3] = [-POPUP_WIDTH * Math.cos(degToRad((-180 * fold) - 90)), POPUP_WIDTH * Math.sin(degToRad((-180 * fold) - 90)), 0];
+		
+		
+		
+		var adj = Math.sqrt(Math.pow(POPUP_WIDTH, 2) - Math.pow(POPUP_WIDTH * Math.sin(degToRad(-15)), 2));
+		points[1] = [-adj * Math.cos(degToRad(-180 * fold)), adj * Math.sin(degToRad(-180 * fold)), POPUP_WIDTH * Math.sin(degToRad(-15))];
+		points[2] = [adj * Math.cos(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-15))];
+		var len = Math.sqrt(Math.pow(points[1][0], 2) + Math.pow(points[1][1], 2) + Math.pow(points[1][2], 2));
+		
+		// normalize the vectors
+		var normV1 = $V([points[1][0] / len, points[1][1] / len, points[1][2] / len]);
+		var normV2 = $V([points[2][0] / len, points[2][1] / len, points[2][2] / len]);
+		var normV3 = $V([points[3][0] / len, points[3][1] / len, points[3][2] / len]);
+		
+		// calculate the cross vector
+		var cross = normV1.cross(normV2);
+		
+		// calculate the cross vector's angle from vector 3
+		var crossAngle = -radToDeg(cross.angleFrom(normV3)) - 90;
+		
+		// transform the shape
+		graphic.css(cssTransformProperty, 'translateY(' + depth + 'px) rotateZ(' + zRot + 'deg) rotateX(' + crossAngle + 'deg)');
 	}
 }
