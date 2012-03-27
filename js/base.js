8c875bf31dcbba4b8656712437b9387752322dd2
John Schulz <JFSIII@JFSIII.org>
2012-03-27 13:04:45 -0400

    Eliminate needless assignment

--- js/base.js
+++ js/base.js
@@ -503,31 +503,27 @@ function Popup(graphic, depth, event) {
 	
 	function setFold(fold) {
 		var points = [];
+		var adj = Math.sqrt(Math.pow(POPUP_WIDTH, 2) - Math.pow(POPUP_WIDTH * Math.sin(degToRad(-15)), 2));
 		
 		// origin
 		points[0] = [0, 0, 0];
 		
 		// left piece: bottom outside
-		points[1] = [-POPUP_WIDTH * Math.cos(degToRad(-180 * fold)), POPUP_WIDTH * Math.sin(degToRad(-180 * fold)), POPUP_WIDTH * Math.sin(degToRad(-15))];
+		points[1] = [-adj * Math.cos(degToRad(-180 * fold)), adj * Math.sin(degToRad(-180 * fold)), POPUP_WIDTH * Math.sin(degToRad(-15))];
 		
 		// right piece: bottom outside
-		points[2] = [POPUP_WIDTH * Math.cos(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-15))];
+		points[2] = [adj * Math.cos(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-15))];
 		
 		// left piece: top inside
 		points[3] = [-POPUP_WIDTH * Math.cos(degToRad((-180 * fold) - 90)), POPUP_WIDTH * Math.sin(degToRad((-180 * fold) - 90)), 0];
 		
-		
-		
-		var adj = Math.sqrt(Math.pow(POPUP_WIDTH, 2) - Math.pow(POPUP_WIDTH * Math.sin(degToRad(-15)), 2));
-		points[1] = [-adj * Math.cos(degToRad(-180 * fold)), adj * Math.sin(degToRad(-180 * fold)), POPUP_WIDTH * Math.sin(degToRad(-15))];
-		points[2] = [adj * Math.cos(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-180 * 0)), POPUP_WIDTH * Math.sin(degToRad(-15))];
-		var len = Math.sqrt(Math.pow(points[1][0], 2) + Math.pow(points[1][1], 2) + Math.pow(points[1][2], 2));
-		
+
 		// normalize the vectors
+		var len = Math.sqrt(Math.pow(points[1][0], 2) + Math.pow(points[1][1], 2) + Math.pow(points[1][2], 2));
 		var normV1 = $V([points[1][0] / len, points[1][1] / len, points[1][2] / len]);
 		var normV2 = $V([points[2][0] / len, points[2][1] / len, points[2][2] / len]);
 		var normV3 = $V([points[3][0] / len, points[3][1] / len, points[3][2] / len]);
-		
+
 		// calculate the cross vector
 		var cross = normV1.cross(normV2);
 		
